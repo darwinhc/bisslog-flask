0762019ab86451d8470412ed146b43c02c3ccb2f
Darwin Herrera C <darwinsherrerac@gmail.com>
2025-06-25 19:02:35 -0500

    fix: Unit tests

--- tests/unit/cli/test_cli.py
+++ tests/unit/cli/test_cli.py
@@ -15,7 +15,7 @@ def test_cli_runs_successfully(mock_parse_args, mock_run):
     mock_args.use_cases_folder_path = "src/uc"
     mock_args.encoding = "utf-8"
     mock_args.secret_key = "secret"
-    mock_args.infra_folder_path = "infra"
+    mock_args.infra_path = "infra"
     mock_args.jwt_secret_key = "jwt"
     mock_parse_args.return_value = mock_args
 
@@ -26,7 +26,7 @@ def test_cli_runs_successfully(mock_parse_args, mock_run):
     mock_run.assert_called_once_with(
         metadata_file="metadata.yml",
         use_cases_folder_path="src/uc",
-        infra_folder_path="infra",
+        infra_path="infra",
         encoding="utf-8",
         secret_key="secret",
         jwt_secret_key="jwt"
